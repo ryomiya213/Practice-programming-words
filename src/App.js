b460754057fc722536ff3909fbec3f473430d6ae
Ryo Miya <ryomiya213@gmail.com>
2020-06-29 22:43:25 +0900

    fetchでJSONファイルの読み込み

--- src/App.js
+++ src/App.js
@@ -4,66 +4,82 @@ export class App {
   constructor() {
     console.log('App.js');
   }
-  
 
   mount() {
-    // TODO ../data/words.jsonから読み込む
-    const allWords = [
-      {
-        "word": "accept",
-        "meaning": "受諾する、受け入れる"
-      },
-      {
-        "word": "access",
-        "meaning": "受諾する、受け入れる"
-      },
-      {
-        "word": "account",
-        "meaning": "アカウント、口座"
-      },
-      {
-        "word": "algorithm",
-        "meaning": "アルゴリズム"
-      },
-      {
-        "word": "allow",
-        "meaning": "可能にする、許可する"
-      },
-      {
-        "word": "alternative",
-        "meaning": "代替の"
-      }
-    ];
+    // Promise
+/*     fetch('../data/words.json')
+      .then((data) => data.json())
+      .then((dataJson) => {
+        const allWords = dataJson;
+        const typingFormElement = document.querySelector('#typing');
+        const meaningElement = document.querySelector('#meaning');
 
-    const typingFormElement = document.querySelector('#typing');
-    const meaningElement = document.querySelector('#meaning');
+        let words = [];
+        let word = '';
+        let meaning = '';
 
-    let words = [];
-    let word = '';
-    let meaning = '';
-
-    [words, word, meaning] = this.popWords(allWords.slice());
-    let wordIndex = 1;
-    meaningElement.innerText = `${word} ${meaning}`;
-    
-    typingFormElement.addEventListener(('input'), () => {
-      console.log(typingFormElement.value);
-      if (typingFormElement.value === word.slice(0, wordIndex)) {
-        if (wordIndex === word.length) {
-          if (words.length === 0) {
-            words = allWords.slice();
+        [words, word, meaning] = this.popWords(allWords.slice());
+        let wordIndex = 1;
+        meaningElement.innerText = `${word} ${meaning}`;
+        
+        typingFormElement.addEventListener(('input'), () => {
+          console.log(typingFormElement.value);
+          if (typingFormElement.value === word.slice(0, wordIndex)) {
+            if (wordIndex === word.length) {
+              if (words.length === 0) {
+                words = allWords.slice();
+              }
+              [words, word, meaning] = this.popWords(words);
+              meaningElement.innerText = `${word} ${meaning}`;
+              typingFormElement.value = '';
+              wordIndex = 1;
+            } else { 
+              wordIndex += 1;
+            }
+          } else {
+            typingFormElement.value = word.slice(0, wordIndex - 1);
           }
-          [words, word, meaning] = this.popWords(words);
-          meaningElement.innerText = `${word} ${meaning}`;
-          typingFormElement.value = '';
-          wordIndex = 1;
-        } else { 
-          wordIndex += 1;
+        });
+      }); */
+
+    // Async Function
+    const loadJson = async() => {
+      const data = await fetch('../data/words.json');
+      const jsonData = await data.json();
+
+      const allWords = jsonData;
+      const typingFormElement = document.querySelector('#typing');
+      const meaningElement = document.querySelector('#meaning');
+
+      let words = [];
+      let word = '';
+      let meaning = '';
+
+      [words, word, meaning] = this.popWords(allWords.slice());
+      let wordIndex = 1;
+      meaningElement.innerText = `${word} ${meaning}`;
+      
+      typingFormElement.addEventListener(('input'), () => {
+        console.log(typingFormElement.value);
+        if (typingFormElement.value === word.slice(0, wordIndex)) {
+          if (wordIndex === word.length) {
+            if (words.length === 0) {
+              words = allWords.slice();
+            }
+            [words, word, meaning] = this.popWords(words);
+            meaningElement.innerText = `${word} ${meaning}`;
+            typingFormElement.value = '';
+            wordIndex = 1;
+          } else { 
+            wordIndex += 1;
+          }
+        } else {
+          typingFormElement.value = word.slice(0, wordIndex - 1);
         }
-      } else {
-        typingFormElement.value = word.slice(0, wordIndex - 1);
-      }
-    });
+      });
+    };
+    
+    loadJson();
 
   }
 
